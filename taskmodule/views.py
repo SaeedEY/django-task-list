03ea8422da7e28e70ddc83b6780b51d9f9d7cdd4
saeed <saeed@example.com>
2024-01-27 17:07:18 +0330

    former tasks definition expanded

--- taskmodule/views.py
+++ taskmodule/views.py
@@ -2,31 +2,31 @@
 from django.http import HttpResponse, JsonResponse
 import json
 from .models import Task
-
-response = {'status':200,'message':{}}
+# from .schemas import MessageSchemaSchema
+# from .schemas import TaskSchema
 
 # Create your views here.
 def intro(request):
+    
+    response = {'status':200,'message':{}} # Should be moved to something more enhanced structure
     response['message'] = "You got it !"
     return JsonResponse(response)
 
 def tasks_index(request):
+    response = {'status':200,'message':{}} # Should be moved to something more enhanced structure
     response['message'] = "You got it !"
     response['result'] =  [ task for task in Task.objects.only('id','name','description','content','created').values()]
     return JsonResponse(response)
 
-def task_add(request):
-    from django.contrib.auth import get_user_model as User
-    if request.POST:
-        try:
-            response['message'] = "You got it !"
-            task = Task(name = request['name'],description = request['description'], owner= get_user_model, bucket= request['bucket'], content= request['bucket'])
-            task.save()
-        except Exception as exp:
-            response['message'] = "Oops"
-            response['status'] = '500'
-            response['result'] = str(exp)
-    else:
-        response['message'] = "request not found"
-        response['status'] = '404'
+def task_add(request, request_data=None):
+    # from django.contrib.auth import get_user_model as User
+    response = {'status':200,'message':{}} # Should be moved to something more enhanced structure
+    # try:
+    task = Task(name = request_data.name, description = request_data.description, owner= request.user.id, bucket= request_data.bucket, content= request_data.bucket) 
+    task.save()
+    response['message'] = "You got it !"
+    # except Exception as exp:
+    #     response['message'] = "Oops"
+    #     response['status'] = '500'
+    #     response['result'] = str(exp)
     return JsonResponse(response)
