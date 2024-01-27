0ed7872622d4107b179dbece85238ad53bb30c13
saeed <saeed@example.com>
2024-01-27 17:04:49 +0330

    tasks index and add route modified

--- taskmodule/api.py
+++ taskmodule/api.py
@@ -1,17 +1,29 @@
 from ninja import NinjaAPI
-from .schemas import TaskSchema
+from ninja.errors import ValidationError, HttpError
+from .schemas import TaskSchema, MessageSchema, AuthenticateSchema
 from . import views
 
 api = NinjaAPI()
+
+# @api.exception_handler(ValidationError)
+# def invalid_request(request, exc):
+#     return MessageSchema(status=)
 
 @api.get('/', response=TaskSchema)
 def intro(request):
     return views.intro(request)
 
+@api.post('/authenticate', response= MessageSchema)
+def login(request, data: AuthenticateSchema):
+    return 
+
 @api.get('/tasks', response=TaskSchema)
 def tasks_index(request):
     return views.tasks_index(request)
 
-@api.post('/task/new', response=TaskSchema)
-def task_add(request):
-    return views.task_add(request)
+@api.post('/task/new', response= MessageSchema)
+def task_add(request , data: TaskSchema):
+    print(request.user)
+    if not request.user.is_authenticated:   # Supposed to moved in some other layers 
+        return MessageSchema(status='403', message='Access denied !')
+    return views.task_add(request, data)
