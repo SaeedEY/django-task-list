3d90e5b72e1e983c98392f977f8843126e1ecb43
saeed <saeed@example.com>
2024-01-31 22:44:00 +0330

    custome user in admin

--- taskmodule/admin.py
+++ taskmodule/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Bucket
-from .models import Task
+from django.contrib.auth.admin import UserAdmin
+from .models import Bucket, Task, Subscriber
+from .forms import SubscriberCreationForm, SubscriberChangeForm
+
+class SubscriberAdmin(UserAdmin):
+    add_form = SubscriberCreationForm
+    form = SubscriberChangeForm
+    model = Subscriber
+    list_display = ["id", "email", "username", "active"]
+
+admin.site.register(Subscriber, SubscriberAdmin)
 
 admin.site.register(Bucket)
 admin.site.register(Task)
