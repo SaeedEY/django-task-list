64e77277abcfd477232a8b1f63f3bb0431dcbe7c
saeed <saeed@example.com>
2024-01-31 22:44:37 +0330

    custome user model and form

---
+++ taskmodule/forms.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import Subscriber
+
+class SubscriberCreationForm(UserCreationForm):
+    class Meta:
+        model = Subscriber
+        fields = ("active", )
+
+    #     name = models.CharField(max_length=64, null=False)
+    # family = models.CharField(max_length=64, null=False)
+    # # email = models.EmailField(max_length=64, null=False, unique=True)
+    # # password = models.CharField(max_length=64, null=False)
+    # active = models.BooleanField(db_default=True)
+    # created = models.DateTimeField(db_default=Now(),editable=False)
+
+class SubscriberChangeForm(UserChangeForm):
+
+    class Meta:
+        model = Subscriber
+        fields = ("active", )
--- taskmodule/models.py
+++ taskmodule/models.py
@@ -1,15 +1,32 @@
 import uuid
-from django.contrib.auth import get_user_model as User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.functions import Now
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class Subscriber(AbstractUser):
+    class Role(models.TextChoices):
+        GUEST = "GST", _('Guest')
+        SUBSCRIBER = "SUB", _('Subsriber')
+        ADMIN = "ADM", _('Admin')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=64, null=False)
+    family = models.CharField(max_length=64, null=False)
+    role = models.CharField(max_length=3, choices=Role, db_default= Role.GUEST)
+    email = models.EmailField(max_length=64, null=False, unique=True)
+    # password = models.CharField(max_length=64, null=False)
+    active = models.BooleanField(db_default=True)
+    created = models.DateTimeField(db_default=Now(),editable=False)
+    def __str__(self):
+        return self.email
+
 class Bucket(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=64, null=False)
     description = models.TextField(max_length=256, blank=True)
     # owner = models.UUIDField(null=False,editable=False)
-    owner = models.ForeignKey(User(), on_delete=models.PROTECT)
+    owner = models.ForeignKey(Subscriber, on_delete=models.PROTECT)
     active = models.BooleanField(db_default=True)
     created = models.DateTimeField(db_default=Now(),editable=False)
 
@@ -18,24 +35,15 @@ class Task(models.Model):
     name = models.CharField(max_length=64, null=False)
     description = models.TextField(max_length=256)
     # owner = models.UUIDField(null=False,editable=False)
-    owner = models.ForeignKey(User(), on_delete=models.PROTECT)
+    owner = models.ForeignKey(Subscriber, on_delete=models.PROTECT)
     # bucket = models.UUIDField(null=False)
     bucket = models.ForeignKey("Bucket", on_delete=models.PROTECT)
     content = models.TextField(max_length=2048,null=False)
     active = models.BooleanField(db_default=True)
     created = models.DateTimeField(db_default=Now(),editable=False)
 
-# class User(User()):
-#     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     name = models.CharField(max_length=64, null=False)
-#     family = models.CharField(max_length=64, null=False)
-#     # email = models.EmailField(max_length=64, null=False, unique=True)
-#     # password = models.CharField(max_length=64, null=False)
-#     active = models.BooleanField(db_default=True)
-#     created = models.DateTimeField(db_default=Now(),editable=False)
-
 class UserBucket(models.Model):
-    user = models.ForeignKey(User(), on_delete=models.PROTECT)
+    user = models.ForeignKey(Subscriber, on_delete=models.PROTECT)
     bucket = models.ForeignKey("Bucket", on_delete=models.PROTECT)
     active = models.BooleanField(db_default=True)
     created = models.DateTimeField(db_default=Now(),editable=False)
