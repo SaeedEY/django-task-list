676e1ed152f0d92c09738f246d0470ba3ca6cc9b
saeed <saeed@example.com>
2024-02-01 23:58:08 +0330

    model serializable to json/dict

--- taskmodule/models.py
+++ taskmodule/models.py
@@ -1,8 +1,15 @@
 import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core import serializers
 from django.db.models.functions import Now
+from django.forms.models import model_to_dict
 from django.utils.translation import gettext_lazy as _
+
+# class Serializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = models
+#         fields = '__all__'
 
 # Create your models here.
 class Subscriber(AbstractUser):
@@ -30,6 +37,17 @@ class Bucket(models.Model):
     owner = models.ForeignKey(Subscriber, on_delete=models.PROTECT)
     active = models.BooleanField(db_default=True)
     created = models.DateTimeField(db_default=Now(),editable=False)
+    
+    def __str__(self):
+        self.model_dump()
+
+    def to_dict(self):
+        return model_to_dict(self)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['name','owner'], name="%(app_label)s_%(class)s_unique")
+        ]
 
 class Task(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
